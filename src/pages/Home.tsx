498b845a7bea740b10a7c3f44ccea3fc725d86e7
RK <68778195+ReuvenKatz@users.noreply.github.com>
2025-07-14 19:18:39 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
               PhD GATEWAY CONSULTING
             </h1>
             <p className="text-lg lg:text-xl text-gray-600 max-w-3xl mx-auto mb-12">
-              Professional guidance from experienced experts Prof. Reuven Katz and EdD. Hila Katz. Advancing careers 
+              Professional guidance from experienced experts Prof. Reuven Katz and Dr. Hila Katz. Advancing careers 
               and doctoral research with individual consulting services.
             </p>
             
